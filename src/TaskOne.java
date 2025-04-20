540c4421212e496707f106f654aacf4be3f5cd8f
Pooja Ramdas <pooja.ramdas05@gmail.com>
2025-04-20 17:32:46 +0530

    Added TaskOne

--- src/TaskOne.java
+++ src/TaskOne.java
@@ -1,2 +1,13 @@
-public class TaskOne {
+// This class performs Task One using Runnable
+
+public class TaskOne implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("TaskOne started.");
+        // Add operation logic here
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("TaskOne: " + i);
+        }
+        System.out.println("TaskOne finished.");
+    }
 }
