b63067d98f61dab46a9495534b221fa547ff163a
Pooja Ramdas <pooja.ramdas05@gmail.com>
2025-04-20 17:34:27 +0530

    Added Main.java

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main thread started.\n");
+
+        // Creating threads for operations
+        Thread t1 = new Thread(new TaskOne());
+        Thread t2 = new Thread(new TaskTwo());
+
+        // Starting the threads
+        t1.start();
+        t2.start();
+
+        // Ensuring all threads complete
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\nMain thread finished.");
+    }
+}
